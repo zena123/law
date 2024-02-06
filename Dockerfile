8ad3c2b1eab457f0013aea6aaaa7001c03f78f63
zena123 <zinab.jom3a@gmail.com>
2024-02-06 17:20:52 +0100

    add django models

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY requirements.txt /law/requirements.txt
 RUN pip install -r requirements.txt
 COPY . /law
 
-CMD python manage.py wait_for_db && python manage.py runserver 0.0.0.0:8000
+CMD python manage.py wait_for_db && python manage.py migrate && python manage.py runserver 0.0.0.0:8000
--- core/models.py
+++ core/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class Client(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone']
+
+    def __str__(self):
+        return self.email
+
+
+class DocumentAttachment(models.Model):
+    document = models.FileField(upload_to='documents/')
+
+
+class LegalRequest(models.Model):
+    STATUS_CHOICES = [
+        ('open', 'Open'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('rejected', 'Rejected'),
+    ]
+
+    case_description = models.TextField(null=True, blank=True)
+    case_type = models.CharField(max_length=255, null=True, blank=True)
+    submission_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='open')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="requests")
+    document_attachment = models.ForeignKey(DocumentAttachment, on_delete=models.CASCADE, related_name="documents")
+
+    def __str__(self):
+        return self.case_type
